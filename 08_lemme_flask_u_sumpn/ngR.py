a0c51317bcba30f35fa60b597c9fbc3200d39804
rng <rng@stuy.edu>
2018-09-20 03:38:21 -0400

    added

--- 08_lemme_flask_u_sumpn/ngR.py
+++ 08_lemme_flask_u_sumpn/ngR.py
@@ -2,3 +2,23 @@
 # SoftDev1 pd7
 # K08 -- Fill Yer Flask
 # 2018-09-19
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def func1():
+      return '<a href="/mom"> Hi Mom! </a><br><a href="/dad"> Hi Dad! </a>'
+
+@app.route("/mom")
+def func2():
+      return '<a href="/dad"> Hi Dad! </a><br><a href="/"> .-.</a>'
+  
+@app.route("/dad")
+def func3():
+      return '<a href="/mom"> Hi Mom! </a><br><a href="/"> .-.</a>'
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
