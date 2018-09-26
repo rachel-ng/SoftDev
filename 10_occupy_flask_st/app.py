31f7f9bf863be27f8744c77aef6702972cf64723
rng <rng@stuy.edu>
2018-09-25 22:55:32 -0400

    halp -> auxiliary

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -3,24 +3,22 @@
 # K10 -- Jinja Tuning
 # 2018-09-25
 
+import util.auxiliary as aux
 from flask import Flask, render_template
-from util import halp
 
 app = Flask(__name__)  # create instance of class Flask
 
 @app.route("/")
 def rootroute():
-    return "<h2>Go to <a href='occupations'>/occupations</a>!</h2>"
+    return "<h2><a href='/occupations'>Click here for /occupations</a></h2>" # link to /occupations
+
 
 # this function is assigned to a route...!
 @app.route("/occupations")
 def showOccupations():
-
     return render_template("occupationsTable.html",
-                           occupations = halp.getOccupationsDict(),  # `occupation` here refers to Jinja var
-                           randomOccupation = halp.getRandomOccupation()
-                           # puts the keys to occupation dict in a list, choose randomly (weighted) from that list
-                           # random.choices() returns a k-sized list (default=1) ... only concerned with first output
+                           occupations = aux.getOccupationsDict(),
+                           randomOccupation = aux.getRandomOccupation()
     )
 
 
@@ -28,8 +26,8 @@ if __name__ == "__main__":
     # must populate dictionary & list with data
     # these methods are not linked to a Flask route,
     # so not necessary that app is running...
-    halp.csvToDict()
-    halp.dictWeights()
+    aux.csvToDict()
+    aux.dictWeights()
     
     app.debug = True
     app.run()
--- 10_occupy_flask_st/util/auxiliary.py
+++ 10_occupy_flask_st/util/auxiliary.py
@@ -11,34 +11,35 @@ weights = []
 
 # POSTCONDITION: occupations dict is populated with occupation data
 def csvToDict():
-    csvFileObject = open('./data/occupations.csv', 'r')  # open the csv file object in 'read' mode
-    readerObject = reader(csvFileObject)  # read the records in the csv
+    csvFileObject = open('./data/occupations.csv', 'r')  # opens csv in 'read' mode
+    readerObject = reader(csvFileObject)  # reads the csv
 
     for record in readerObject:
         if record[0] == "Job Class" or record[0] == "Total":  
-            continue  # skip the first and last records
+            continue  # skips first and last records
         
-        # record[0] is occupation string, record[1] is floating point (as a string), record[2] is link
-        # must convert the appropriate string into floating point before placing as value in dict
-        # occupation : [ <percentage> , <link> ]
+        # record[0] = occupation (string), record[1] = percentage (string), record[2] = link (string)
+        # converts percentage into floating point
         else:
             occupations[ record[0] ] = [ float( record[1] ) , record[2] ]
+            # occupation : [ <percentage> , <link> ]
             
     csvFileObject.close()
 
-#  PRECONDITION: occupations dict cannot be empty (csvToDict must have ran first)
+# PRECONDITION: occupations dict cannot be empty (csvToDict must have ran first)
 # POSTCONDITION: weights list is populated w/ weighted percentages of randomness
 def dictWeights():
-    listValues = list(occupations.values()) # list containing a list of the values in occupations dict
-    # the values themselves are lists of the form [ <percentage> , <link> ]
+    listValues = list(occupations.values()) # list containing list of values in occupations dict
+    # values in form [ <percentage> , <link> ]
 
     for listOfOccupationData in listValues:             # for each value in current list of occupation data
-        weights.append( listOfOccupationData[0] )       # put the percentage in list of weights
+        weights.append( listOfOccupationData[0] )       # add percentage to list of weights
 
 # these accessor methods are necessary b/c
 # it doesn't make sense to keep reading the csv file & re-populate the occupations dict
 # every time we want to access the data
-#
+
+# accessor methods so we don't need to reread csv file and repop occupations dict everytime we want to access the data
 # POSTCONDITION (both functions): occupations{} and weights[] must be populated w/ data
 def getOccupationsDict():
     return occupations
@@ -48,3 +49,8 @@ def getWeights():
 
 def getRandomOccupation():
     return choices( list(getOccupationsDict().keys()) , weights = getWeights() )[0]
+# puts the keys to occupation dict in a list, choose randomly (weighted) from that list
+# random.choices() returns a k-sized list (default=1) ... only concerned with first output
+
+#choices( list(getOccupationsDict().keys()) , weights = getWeights() )[0]
+
