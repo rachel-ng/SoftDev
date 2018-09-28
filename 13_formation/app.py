7d63b52846dc32e8ede3acee5f7f55e03a188e0b
rng <rng@stuy.edu>
2018-09-28 01:14:48 -0400

    i think it works. i hope it works

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)  # create instance of class Flask
 
 import cgi
+import util.auxiliary as aux
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    print(app)
+    return render_template("forms.html", title = "hi")
 
 @app.route("/auth")
 def authenticate():
@@ -10,14 +16,14 @@ def authenticate():
     print(request.args)
     print(request.args['username'])
     print(request.headers)
-    return "hi " + request.args['username'] + "<br> your request method was" + request
-#render_template("template.html", title = "hi", username = request.args['username'])
+    return render_template("template.html",
+                           title = "hi",
+                           username = request.args['username'],
+                           rmethod = request.method,
+                           fullpath = request.full_path,
+                           foody = aux.moodyfoody())
+    # return "hi " + request.args['username'] + "<br>your request method was " + request.method + "<br> your full path is " + request.full_path
 
-@app.route("/")
-def home():
-    #coll = [0,1,1,2,3,5,8]
-    print(app)
-    return render_template("forms.html", title = "hi") #, coll = coll)
 
 if __name__ == "__main__":
     app.debug = True
