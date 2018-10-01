6a55807d1321485a602a5cdc7eb6d006645466f4
rng <rng@stuy.edu>
2018-10-01 18:57:29 -0400

    what happened to the cookies ;~;

--- 14_login/app.py
+++ 14_login/app.py
@@ -4,24 +4,51 @@ from config import sec_key
 app = Flask(__name__)
 app.secret_key = sec_key
 
-@app.route("/")
-def home():
-    return render_template("login.html")
+un = "bob"
+pw = "kazi"
 
+@app.route("/")
+def root():
+    print ("root")
+    return render_template("login.html")
+            
 @app.route("/login", methods=["POST"])
 def login():
-    if request.form['username'] != 'bob':
+    print ("login")
+    print(request.form["username"])
+    if request.form["username"] != un and request.form["passwd"] != pw:
+        session["msg"] = "wrong username and password"
+        return redirect(url_for("error"))
+    if request.form["username"] != un:
         session["msg"] = "wrong username"
         return redirect(url_for("error"))
+    if request.form["passwd"] != pw:
+        session["msg"] = "wrong password"
+        return redirect(url_for("error"))
     else:
-        session["username"] = request.cookies.get("username")
-    return render_template("home.html")
+        session["username"] = request.form["username"] #request.cookies.get("username")
+        session["passwd"] = request.form["passwd"] #request.cookies.get("passwd")
+    return redirect(url_for("welcome"))
 
 @app.route("/error")
 def error():
-    # msg = "wrong username"
+    print ("oof - an error")
     return render_template("error.html", error_message = session["msg"])
-    
+
+@app.route("/home")
+def welcome():
+    print ("help, i've fallen and i can't get up")
+    return render_template("logout.html",
+                           username = session["username"])
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    print ("byeeeeeee")
+    if request.form["sub1"] == "Logout":
+        session.pop("username")
+        session.pop("passwd")
+        return render_template("home.html")
+
 
 if __name__ == "__main__":
 	app.debug = True
