22e5b9f96dc189ce320ef13581b2098c65a1ef81
rng <rng@stuy.edu>
2018-10-02 17:02:20 -0400

    flashes error messages to login page now

--- 15_flashinh/app.py
+++ 15_flashinh/app.py
@@ -1,7 +1,7 @@
 # Team Riiiii -- Rachel Ng and Ivan Zhang
 # SoftDev1 pd7
-# K14 -- Do I Know You?
-# 2018-10-01
+# K15 -- Oh yes, perhaps I do… 
+# 2018-10-02
 
 from flask import Flask, render_template, request, session, url_for, redirect, flash
 from config import sec_key
@@ -24,45 +24,33 @@ def home():
     p("\nhome")
     p(session)
     
-    if len(session.keys()) > 0: # if session isn't empty, return home page w/ greeting
-        return render_template("logout.html", username = session["username"])
+    if len(session.keys()) > 0 and "username" in session: # if session contains username, return home page w/ greeting
+        return render_template("logout.html", user = session["username"])
     return render_template("login.html") # return login page
             
 @app.route("/login", methods=["POST"])
 def login():
     p("\nlogging in...\tuser: " + request.form["username"])
     
-    if request.form["username"] != usr and request.form["passwd"] != pw: # both wrong error page
+    if request.form["username"] != usr and request.form["passwd"] != pw: # flashes both wrong error
         flash("Wrong username and password")
-        #return redirect(url_for("login"))
-    elif request.form["username"] != usr: # wrong username error page
+    elif request.form["username"] != usr: # flashes wrong username error
         flash("Wrong username")
-        #return redirect(url_for("login"))
-    elif request.form["passwd"] != pw: # wrong password error page
+    elif request.form["passwd"] != pw: # flashes wrong password error
         flash("Wrong password")
-        #return redirect(url_for("login"))
     else: # adds username and password to session, no cookies ;~;
         session["username"] = request.form["username"] 
         session["passwd"] = request.form["passwd"]
     p(session)
     return redirect(url_for("home")) # return home page
 
-@app.route("/error")
-def error():
-    p("\noof - an error has occured: " + session["msg"])
-    
-    problem = session["msg"] # stores error message
-    session.clear() # clears session so we can try logging in again
-    return render_template("error.html", error_message = problem) # return error page
-
 @app.route("/logout", methods=["POST"])
 def logout():
-    p("\nlogging out...")
+    p("\nlogging out..")
     
     if request.form["sub1"] == "Logout": # logout button
         session.clear() # clears session
         return render_template("home.html") # return home page
-
 
 if __name__ == "__main__":
 	app.debug = True
