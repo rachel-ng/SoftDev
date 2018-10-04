469e5214168ca9f2bd69877d3cf8fbc417cc36a2
rng <rng@stuy.edu>
2018-10-04 17:06:00 -0400

    added some stuff to make it easier for lazy people (aka me)

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -3,51 +3,48 @@
 # K16 -- No Trouble    or Treble
 # 2018-10-04
 
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
+import sqlite3 # control sqlite db
+import csv # csv I/O
+
+import os # removes rv.db so we don't get 'sqlite3.OperationalError: table courses already exists'
+if os.path.isfile("rv.db"): # removes rv.db if it exists so we don't get 
+    os.remove("rv.db") # 'sqlite3.OperationalError: table courses already exists'
+else:
+    pass # doesn't do anything if rv.db doesn't exist
 
 
+# python db_builder.py; sqlite3 rv.db
+# SELECT * FROM courses;
+# SELECT * FROM peeps;
 
-pri = True
+
+pri = False
 
 def p(nt): # turn printing messages on and off (diagnostics)
     if pri == True: 
         print (nt)
 
-# rm rv.db; python db_builder.py; sqlite3 rv.db
-# sqlite3 rv.db
-# SELECT * FROM courses;
-# SELECT * FROM peeps;
-
-
 
 DB_FILE="rv.db"
 
-db = sqlite3.connect(DB_FILE)
-c = db.cursor()
+db = sqlite3.connect(DB_FILE) # opens or makes the file
+c = db.cursor() 
 
 
-
-# opens courses.csv and makes a dictionary
+# opens courses.csv and makes a dictionary (courses)
 csv1 = open('courses.csv', 'r')
 courses = csv.DictReader(csv1)
-#for row in courses:
-    # print (row)
 
-# opens peeps.csv and makes a dictionary
+# opens peeps.csv and makes a dictionary (peeps)
 csv2 = open('peeps.csv', 'r')
 peeps = csv.DictReader(csv2)
-#for row in peeps:
-    # print(row)
-
-
 
 # makes courses table in rv.db
 command = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
 p("\n" + command) 
 c.execute(command)
 
-for row in courses: # adds code, id, and mark
+for row in courses: # adds code, id, and mark to courses
     adding = "INSERT INTO courses VALUES ('" + row['code'] + "', " + row['id'] + ", " + row['mark'] + ");"
     p(adding)
     c.execute(adding)
@@ -57,12 +54,13 @@ command = "CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
 p("\n" + command)
 c.execute(command)
 
-for row in peeps: # adds name, id, and age
+for row in peeps: # adds name, id, and age to peeps
     adding = "INSERT INTO peeps VALUES ('" + row['name'] + "', " + row['id'] + ", " + row['age'] + ");"
     p(adding)
     c.execute(adding)
 
-db.commit()
-db.close()
+
+db.commit() # saves changes
+db.close() # closes db
 
 
