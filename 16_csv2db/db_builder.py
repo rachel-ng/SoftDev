38305a16dbed2e252f7816f493441af973a5ed13
rachel-ng <rng@stuy.edu>
2018-10-04 13:18:06 -0400

    something works?

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -7,31 +7,33 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
 
-#DB_FILE="discobandit.db"
+DB_FILE="discobandit.db"
 
-#db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-#c = db.cursor()               #facilitate db ops
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
 
 csv1 = open('courses.csv', 'r')
 courses = csv.DictReader(csv1)
-for row in courses:
-    print(row)
+#for row in courses:
+    #print(row)
 
 csv2 = open('peeps.csv', 'r')
 peeps = csv.DictReader(csv2)
-for row in peeps:
-    print(row)
+#for row in peeps:
+    #print(row)
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
 
-#command = ""          #build SQL stmt, save as string
-#c.execute(command)    #run SQL statement
+command = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
+command2 = "INSERT INTO courses VALUES ('class', 1, 1);"          #build SQL stmt, save as string
+c.execute(command)    #run SQL statement
+c.execute(command2)
 
 #==========================================================
 
-#db.commit() #save changes
-#db.close()  #close database
+db.commit() #save changes
+db.close()  #close database
 
 
