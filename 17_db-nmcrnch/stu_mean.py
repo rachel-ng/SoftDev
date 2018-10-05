9a3230333a6d072a5773b3bf348b79a9f374797a
rachel-ng <rng@stuy.edu>
2018-10-05 13:17:09 -0400

    17

---
+++ 17_db-nmcrnch/stu_mean.py
@@ -0,0 +1,72 @@
+# Team Where's Perry? -- Puneet Johal, Rachel Ng
+# SoftDev1 pd7
+# K17 -- Average
+# 2018-10-04
+
+# python db_builder.py; sqlite3 rv.db
+# SELECT * FROM courses;
+# SELECT * FROM peeps;
+
+import sqlite3 # control sqlite db
+import csv # csv I/O
+
+import os
+if os.path.isfile("rv.db"): # removes rv.db if it exists so we don't get 
+    os.remove("rv.db") # 'sqlite3.OperationalError: table courses already exists'
+else:
+    pass # doesn't do anything if rv.db doesn't exist
+
+
+pri = False
+
+def p(nt): # turn printing messages on and off (diagnostics)
+    if pri == True: 
+        print (nt)
+
+
+DB_FILE = "rv.db"
+
+db = sqlite3.connect(DB_FILE) # opens or makes the file
+c = db.cursor() 
+
+
+# opens courses.csv and makes a dictionary (courses)
+csv1 = open('courses.csv', 'r')
+courses = csv.DictReader(csv1)
+
+# opens peeps.csv and makes a dictionary (peeps)
+csv2 = open('peeps.csv', 'r')
+peeps = csv.DictReader(csv2)
+
+# makes courses table in rv.db
+command = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
+p("\n" + command) 
+c.execute(command)
+
+for row in courses: # adds code, id, and mark to courses
+    adding = "INSERT INTO courses VALUES ('" + row['code'] + "', " + row['id'] + ", " + row['mark'] + ");"
+    p(adding)
+    c.execute(adding)
+
+# makes peeps table in rv.db
+command = "CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
+p("\n" + command)
+c.execute(command)
+
+for row in peeps: # adds name, id, and age to peeps
+    adding = "INSERT INTO peeps VALUES ('" + row['name'] + "', " + row['id'] + ", " + row['age'] + ");"
+    p(adding)
+    c.execute(adding)
+
+c.execute("SELECT id FROM peeps")
+peeps_ids = c.fetchall()
+print (peeps_ids)
+
+c.execute("SELECT peeps.id, peeps.name, courses.mark FROM peeps, courses WHERE peeps.id = courses.id;")
+print c.fetchall()
+
+
+db.commit() # saves changes
+db.close() # closes db
+
+
