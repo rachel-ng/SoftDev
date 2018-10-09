be3467af4c2d9037415b6829d1136c80e7c7abee
rng <rng@stuy.edu>
2018-10-09 17:45:00 -0400

    using params in this

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -3,18 +3,20 @@
 # K17 -- Average
 # 2018-10-04
 
-# python db_builder.py; sqlite3 rv.db
+# python stu_mean.py; sqlite3 rip.db
 # SELECT * FROM courses;
 # SELECT * FROM peeps;
+# SELECT * FROM peeps_avg;
+
 
 import sqlite3 # control sqlite db
 import csv # csv I/O
 
 import os
-if os.path.isfile("rv.db"): # removes rv.db if it exists so we don't get 
-    os.remove("rv.db") # 'sqlite3.OperationalError: table courses already exists'
+if os.path.isfile("rip.db"): # removes rip.db if it exists so we don't get 
+    os.remove("rip.db") # 'sqlite3.OperationalError: table courses already exists'
 else:
-    pass # doesn't do anything if rv.db doesn't exist
+    pass # doesn't do anything if rip.db doesn't exist
 
 
 pri = True
@@ -23,7 +25,7 @@ def p(nt): # turn printing messages on and off (diagnostics)
     if pri == True: 
         print (nt)
 
-DB_FILE = "rv.db"
+DB_FILE = "rip.db"
 
 db = sqlite3.connect(DB_FILE) # opens or makes the file
 c = db.cursor() 
@@ -33,30 +35,41 @@ c = db.cursor()
 csv1 = open('courses.csv', 'r')
 courses = csv.DictReader(csv1)
 
-# opens peeps.csv and makes a dictionary (peeps)
-csv2 = open('peeps.csv', 'r')
-peeps = csv.DictReader(csv2)
-
-# makes courses table in rv.db
+# makes courses table in rip.db
 command = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
 #p("\n" + command) 
 c.execute(command)
 
-for row in courses: # adds code, id, and mark to courses
-    adding = "INSERT INTO courses VALUES ('" + row['code'] + "', " + row['id'] + ", " + row['mark'] + ");"
-    #p(adding)
-    c.execute(adding)
+for row in courses: # adds code, id, and mark to courses    
+    params = (row['code'], row['id'], row['mark'])
+    c.execute("INSERT INTO courses VALUES (?,?,?)", params)
 
-# makes peeps table in rv.db
+
+# opens peeps.csv and makes a dictionary (peeps)
+csv2 = open('peeps.csv', 'r')
+peeps = csv.DictReader(csv2)
+
+# makes peeps table in rip.db
 command = "CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
 #p("\n" + command)
 c.execute(command)
 
 for row in peeps: # adds name, id, and age to peeps
-    adding = "INSERT INTO peeps VALUES ('" + row['name'] + "', " + row['id'] + ", " + row['age'] + ");"
-    #p(adding)
-    c.execute(adding)
+    params = (row['name'], row['id'], row['age'])
+    c.execute("INSERT INTO peeps VALUES (?,?,?)", params)
 
+
+# for calculating averages
+def calcAvg(): 
+    for s in peeps_grades: # gets avg and puts in peeps_avg
+        grade = 0.0
+        div = 0
+        for m in peeps_grades[s]:
+            grade += m
+            div += 1
+        peeps_avg[s] = [grade / div, div] # [average, num classes]
+    #p(peeps_avg)    
+    #p(peeps_names)
 
 
 peeps_avg = {} # id : avg
@@ -75,34 +88,24 @@ for student in c.fetchall(): # make dictionaries
 c.execute("SELECT id, mark FROM courses;") # get ids and grades from courses
 for g in c.fetchall(): # add list of grades to peeps_grades
     peeps_grades[g[0]].append(g[1])
-p(peeps_grades)
+#p(peeps_grades)
 
-for s in peeps_grades:
-    grade = 0.0
-    div = 0
-    for m in peeps_grades[s]:
-        grade += m
-        div += 1
-    p(s)
-    peeps_avg[s] = [grade / div, div] # [average, num classes]
-    
-p(peeps_avg)    
-p(peeps_names)
 
-for peep in peeps_names:
-    print "student #" + str(peep) + ", " + peeps_names[peep] + ": " + str(peeps_avg[peep][0])
     
-# makes peeps_avg table in rv.db
+# makes peeps _avg table in rip.db
 command = "CREATE TABLE peeps_avg (id INTEGER, avg INTEGER);"
-p("\n" + command)
+#p("\n" + command)
 c.execute(command)
 
-for peep in peeps_grades:
-    p("peep")
-    p(peep)
-    adding = "INSERT INTO peeps_avg (" + str(peep) + ", " + str(peeps_avg[peep]) + ");"
-    p(adding)
-    c.execute(adding)
+calcAvg()
+
+for s in peeps_avg:
+    params = (s,peeps_avg[s][0])
+    c.execute("INSERT INTO peeps_avg VALUES (?,?)", params)
+
+
+for peep in peeps_names: # prints peeps id name avg
+    print str(peep) + "\t" + peeps_names[peep] + ": " + str(peeps_avg[peep][0])
 
 
 db.commit() # saves changes
