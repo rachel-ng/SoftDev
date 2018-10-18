3e38a75ff30ef2ceacda140b4f16d661902d71a8
rng <rng@stuy.edu>
2018-10-17 22:41:37 -0400

    fixed it

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -3,21 +3,25 @@ app = Flask(__name__)  # create instance of class Flask
 
 import cgi
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
     print(app)
     return render_template("forms.html", title = "hi")
 
-@app.route("/auth")
+@app.route("/auth", methods=["GET", "POST"])
 def authenticate():
     print(app)
     print(request)
     print(request.args)
-    print(request.args['username'])
-    print(request.headers)
+
+    m = {}
+    if request.method == "GET":
+        m = request.args
+    if request.method == "POST":
+        m = request.form
     return render_template("template.html",
                            title = "hi",
-                           username = request.args['username'],
+                           username = m['username'],
                            rmethod = request.method)#,
                           # fullpath = request.full_path)
     # return "hi " + request.args['username'] + "<br>your request method was " + request.method + "<br> your full path is " + request.full_path
