8ad125ed169dc4faad909368306be800980f19dd
rng <rng@stuy.edu>
2018-11-15 04:50:30 -0500

    i've rickrolled myself too much trying to get this to work so imma just leave it be aight

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -36,7 +36,41 @@ def root():
                            available = trigger,
                            url = d['url'],
                            timestamp = d['timestamp'])
+
+@app.route("/rr/<url>", methods=['GET', 'POST'])
+def rr(url):
+    site = "/"
+    trigger = False
     
+    if (request.form.get('site') != None):
+        site = request.form.get('site')
+        trigger = True
+
+    URL = URL_STUB + URL_QUERY + site
+        
+    response = urllib.request.urlopen(URL)
+    archived = json.loads(response.read())['archived_snapshots']
+
+    if archived:
+        d = archived['closest']
+    else:
+        d = {"available": False, "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "timestamp": "YYYYMMDDhhmmss", "status": "400"}
+        
+    print(list(d.keys()))
+    print(d['url'])
+
+    if (not(trigger)):
+        return redirect('{url}'.format(url=d['url']))
+    else:
+        return render_template("base.html",
+                           title = "The Wayback Machine",
+                           available = trigger,
+                           url = d['url'],
+                           timestamp = d['timestamp'])
+
+
+
+
 #@app.route("/<time>")
 #def root_time(time):
 #    print(time)
